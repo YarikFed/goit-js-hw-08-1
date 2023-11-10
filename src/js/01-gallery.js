3315dad83c794a881bfe7c4210be2f77a204dc4a
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-11-10 22:33:41 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+                  <a class="gallery__link" href="${item.original}">
+                    <img
+                      class="gallery__image"
+                      src="${item.preview}" 
+                      data-source="${item.original}" 
+                      alt="${item.description}"
+                    />
+                  </a>
+                </div>`
+    )
+    .join('');
+}
+
+const addGallaryMarkup = createGallaryMarkup(galleryItems);
+
+divRef.innerHTML = addGallaryMarkup;
+
+divRef.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const galleryItems = galleryItems.map(item => {
+    return {
+      src: item.original,
+      title: item.description
+    };
+  });
+
+  const instance = new SimpleLightbox(galleryItems, { /* опції, якщо потрібно */ });
+  instance.show();
+  
+  divRef.addEventListener('keydown', (evt) => {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+
